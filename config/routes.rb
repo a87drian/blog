7fabcacc7435b1321c45a69f36639666f21256bd
chavesa <adrian.chaves@gmail.com>
2018-08-02 18:00:36 -0300

    voy por las tablas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'article/new'
-  get 'article/show'
+  get 'articles/new'
+  get 'articles/show'
+  get 'articles/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "welcome/index"
   get "welcome/contactos"
