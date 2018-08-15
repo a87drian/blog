a669203a87151d61f7198c715ba0a6332a7ccca4
Adrian <adrian.chaves@gmail.com>
2018-08-15 07:07:51 -0300

    lo ultimo es mensajes desde el controlador

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'articles/new'
-  get 'articles/show'
-  get 'articles/index'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "welcome/index"
   get "welcome/contactos"
