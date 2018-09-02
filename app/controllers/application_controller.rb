10b63fcd8695ff85f64db5c39e60cce61bf28fe7
Adrian <adrian.chaves@gmail.com>
2018-09-02 11:55:35 -0300

    devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :cargar_registros
+  def cargar_registros
+    @categorias = Category.all
+    #code
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# This file is auto-generated from the current state of the database. Instead
+ # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_19_153144) do
+ActiveRecord::Schema.define(version: 2018_08_30_235258) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -24,6 +24,24 @@ ActiveRecord::Schema.define(version: 2018_08_19_153144) do
     t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.string "name"
+    t.string "last_name"
+    t.integer "permission_level"
+    t.string "uid"
+    t.string "provider"
+    t.string "hobby"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
